67973ab344de9d81657df91429ced7d00ae88f44
Lucas Pompeu <lucaspompeuneves@gmail.com>
2021-02-09 16:58:18 -0300

    Aula 01

--- BancoBytebank/Program.cs
+++ BancoBytebank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BancoBytebank.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace BancoBytebank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "454.569.895.-85";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "546.896.452-56";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total bonificações: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
